1ee0920c07b3e6d901d040bc7afc4ac22ee9da46
Jaycen <saitakusei@gmail.com>
2024-04-12 16:50:42 +0900

    Add list for graphql response fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,25 +42,28 @@ export default function CustomizedInputBase() {
   // }, []); 
 
   return (
-    <Paper
-      component="form"
-      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center'}}
-    >
-      <IconButton sx={{ p: '10px' }} aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Input the name of the mansion house"
-        inputProps={{ 'aria-label': 'search mansion trends' }}
-        onBlur={getAllMansions}
-        // value={value}
-      />
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton type="button" color="primary" sx={{ p: '10px' }} aria-label="search">
-        <SearchIcon />
-      </IconButton>
+    <div>
+      <Paper
+        component="form"
+        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center'}}
+      >
+        <IconButton sx={{ p: '10px' }} aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Input the name of the mansion house"
+          inputProps={{ 'aria-label': 'search mansion trends' }}
+          onBlur={getAllMansions}
+          // value={value}
+        />
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <IconButton type="button" color="primary" sx={{ p: '10px' }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        
+      </Paper>
       <MansionList mansions={mansions} />
-    </Paper>
+    </div>
   );
 }
